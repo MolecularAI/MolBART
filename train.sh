d8e05bdf3d99132f237d93a8922a747c668760d6
Ross Irwin <ross.irwin@astrazeneca.com>
2021-03-16 20:17:11 +0100

    Update train.sh script with larger model size and new schedule

--- train.sh
+++ train.sh
@@ -3,6 +3,13 @@ python -m molbart.train \
  --dataset zinc \
  --data_path ../data/zinc \
  --model_type bart \
- --lr 0.001 \
+ --lr 1.0 \
+ --schedule transformer \
+ --d_model 512 \
+ --num_layers 6 \
+ --num_heads 8 \
+ --d_feedforward 2048 \
  --gpus 2 \
- --epochs 1
+ --epochs 1 \
+ --task mask_aug \
+ --batch_size 64
