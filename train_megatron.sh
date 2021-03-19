73d392a533a30a75c942d0d0c5f969513a08c005
sirelkhatim <sirelkhatim_hamid1990@hotmail.com>
2021-03-19 11:47:25 +0100

    fixed spacing train_megatron.sh

--- train_megatron.sh
+++ train_megatron.sh
@@ -8,7 +8,7 @@ if [ -z "$MASTER_PORT" ]
 then
       MASTER_PORT=6000
 else
-      MASTER_PORT= $MASTER_PORT
+      MASTER_PORT=$MASTER_PORT
 fi
 
 export NNODES=$SLURM_JOB_NUM_NODES
@@ -26,7 +26,7 @@ if [ -z "$NODE_RANK" ]
 then
       NODE_RANK=0
 else
-      NODE_RANK= $SLURM_NODEID
+      NODE_RANK=$SLURM_NODEID
 fi
 #export dataset_path=$1
 export megatron_config_path=$1
